48ebe254962bcfb7de472a9b0d57d060345ed9e9
Copypeng <pengyongouc@gmail.com>
2014-08-08 17:51:59 +0800

    add the velocity modules and a new transition.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,9 @@ seajs.config({
 
     alias:{
         'jquery':'/assets/scripts/sea-modules/jquery/1.8.3/jquery-cmdized',
-        'tooltips': '/assets/scripts/tooltips',
-        'tipper': '/assets/scripts/sea-modules/tipper/jquery.fs.tipper'
+        'tipper': '/assets/scripts/sea-modules/tipper/jquery.fs.tipper',
+        'velocity' :'/assets/scripts/sea-modules/velocity/jquery.velocity',
+        'velocity-ui' :'/assets/scripts/sea-modules/velocity/velocity.ui',
     }
 
 });
